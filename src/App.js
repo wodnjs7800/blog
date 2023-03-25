7c469b6fe9ca875926d72a68adb6eca48fc6c507
wodnjs7800 <wodnjs7800@naver.com>
2023-03-26 00:56:48 +0900

    version 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [change, setChange] = useState(['남자코트 추천' ,'강남 우동맛집', '파이썬독학']);
+  const [good, setGood] = useState([0,0,0]);
+  const [modal, setModal] = useState(false);
+  const [title, setTitle] = useState(1);
+  const [input, setInput] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>RactBlog</h4>
+      </div>
+
+      <button onClick={()=>{
+        let copy = [...change];
+        copy[0]='여자코트 추천';
+        setChange(copy);
+        }}>글수정</button>
+
+      <button onClick={()=>{
+        let copy = [...change];
+        copy.sort();
+        setChange(copy);
+        }}>가나다순정렬</button>
+
+      {
+        change.map(function(a,i){
+          return(
+            <div className="list" key={i}>
+              <h4 onClick={()=>{setModal(!modal); setTitle(i)}}>{a}
+               <span onClick={(e)=>{
+                e.stopPropagation();
+                let copy = [...good];
+                copy[i]=copy[i]+1;
+                setGood(copy);
+                }}>👍</span> {good[i]} </h4>
+              <p>3월 24일 발행</p>
+              <button onClick={()=>{
+                let copy = [...change];
+                copy.splice(i, 1);
+                setChange(copy);
+              }}>글삭제</button>
+            </div>
+          )
+        })
+      }
+        
+      <input onChange={(e)=>{setInput(e.target.value)}}></input>
+      <button onClick={()=>{
+        let copy = [...change];
+        copy.unshift(input);
+        setChange(copy);
+      }}>글생성</button>
+      
+
+      {modal === true ? 
+        <Modal 
+          change={change} title={title}
+        /> : null}
+      
     </div>
-  );
+  )
+}
+
+function Modal(props){
+  return(
+    <div className="modal">
+      <h4>제목: {props.change[props.title]}</h4>
+      <p>날짜: </p>
+      <p>상세내용 : </p>
+      
+    </div>
+  )
 }
 
 export default App;
